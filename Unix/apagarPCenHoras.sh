2e77509497e345667cf26cf46f3a036cd0b11a35
Marcelo T.M <marcelo.torrejn@gmail.com>
2018-02-04 18:11:34 +0100

    Update apagarPCenHoras.sh

--- Unix/apagarPCenHoras.sh
+++ Unix/apagarPCenHoras.sh
@@ -1,22 +1,24 @@
-#! /bin/bash
+#!/bin/bash
 clear
 echo "########################################"
 echo "## Apagar el equipo en un tiempo dado ##"
 echo "## por Marcelo T.M.                   ##"
 echo "########################################"
 echo "Selecciona una opcion:"
-echo.
 echo "1. 1h"
 echo "2. 2h"
 echo "3. 3h"
 echo "4. Personalizado"
 echo "5. Una hora concreta(formato 24h)"
-read -n 1 -p "Introduce una opcion" opcion
+read -n 1 -p "Introduce una opcion: " opcion;
+echo -e "\n"
 case $opcion in
-1|2|3) shutdown -h $opcion*60 "Se va a apagar el equipo en " $opcion " horas";;
-4) 	read -n 1 -p "Introduce el tiempo en horas deseado:" horas
-	shutdown -h $opcion*60 "Se va a apagar el equipo en " $horas " horas";;
-5) 	read -n 1 -p "Introduce la hora en formato 24h deseada:" horaConcreta
-	shutdown -h $horaConcreta "Se va a apagar el equipo a las " $horaConcreta " horas";;
+1|2|3) sudo shutdown -h +`expr $opcion '*' 60` "Se va a apagar el equipo en " $opcion " horas";;
+4) 	read -n 1 -p "Introduce el tiempo en horas deseado:" horas;
+	echo -e "\n"
+	sudo shutdown -h +`expr $horas '*' 60` "Se va a apagar el equipo en " $horas " horas";;
+5) 	read -p "Introduce la hora en formato 24h deseada:" horaConcreta;
+	echo -e "\n"
+	sudo shutdown -h $horaConcreta "Se va a apagar el equipo a las " $horaConcreta " horas";;
 *) echo "No has introducido una de las opciones" ;;
 esac
